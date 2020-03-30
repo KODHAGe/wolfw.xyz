33a401f505bd68cfe94f8c8a3c5e3af0225de703
Wolf <wolf.wikgren@gmail.com>
2020-03-30 15:58:12 +0300

    fix typo / dumb error

--- src/components/cvList.js
+++ src/components/cvList.js
@@ -1,7 +1,7 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import CvListItem from "./CvListItem.js"
+import CvListItem from "./cvListItem"
 
 import style from "./cvList.module.css"
 
